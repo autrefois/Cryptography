f5e6ef7f29db4ec293f924b54601286a957bd9c1
autrefois <odette.duma@gmail.com>
2014-05-19 15:25:47 +0300

    initial Submit

---
+++ Classical Cryptography/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Permutation_Cypher
+{
+    public partial class Form1 : Form
+    {
+
+        int n, m;
+        List<int> l1;
+        List<int> l2;
+        List<char> alpha;
+
+        public List<E> ShuffleList<E>(List<E> inputList)
+        {
+            List<E> randomList = new List<E>();
+
+            Random r = new Random();
+            int randomIndex = 0;
+            while (inputList.Count > 0)
+            {
+                randomIndex = r.Next(0, inputList.Count); //Choose a random object in the list
+                randomList.Add(inputList[randomIndex]);   //add it to the new, random list
+                inputList.RemoveAt(randomIndex);          //remove to avoid duplicates
+            }
+
+            return randomList; //return the new random list
+        }
+
+        public int index(int e, List<int> l)
+        {
+            for (int i = 0; i < l.Count; i++)
+                if (e == l[i])
+                    return i;
+            return -1;
+        }
+
+        public string keyAsString(List<int> l)
+        {
+            string s = "";
+            for (int i = 0; i < l.Count; i++)
+                s = s + l[i];
+            return s;
+        }
+
+        public Boolean valid(char e)
+        {
+            bool ok = false;
+            for (int i = 0; i < alpha.Count; i++)
+                if (e == alpha[i])
+                    ok = true;
+            return ok;
+        }
+
+        public void loadDefaultAlphabet()
+        {
+            //default alphabet with 27 characters, and the order of permutation 5.
+            n = 27;
+            m = 5;
+            l1 = new List<int>(m);
+            l2 = new List<int>(m);
+
+            for (int i = 0; i < m; i++)
+                l1.Add(i + 1);
+
+            l2 = ShuffleList<int>(l1);
+
+            alpha = new List<char>(n);
+            alpha.Add(' ');
+            for (int i = 1; i < n - 1; i++)
+                alpha.Add((char)(96 + i));
+
+            txtKey.Text = keyAsString(l2);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (defaultCheckbox.Checked)
+            {
+                txtn.ReadOnly = true;
+                txtm.ReadOnly = true;
+                txtalpha.ReadOnly = true;
+                loadButton.Enabled = false;
+
+                loadDefaultAlphabet();
+            }
+
+            else if (!defaultCheckbox.Checked)
+            {
+                txtn.ReadOnly = false;
+                txtm.ReadOnly = false;
+                txtalpha.ReadOnly = false;
+                txtKey.Text = "";
+            }
+
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+                //take the text as a string of characters and break it into small "lists" of m elements,
+                //then, using l2, return a result.
+
+           string s = txtIn.Text.ToLower();
+
+           while (s.Length % m != 0)  //add spaces if the number of characters is not divisible by 5
+                 s = s + " ";         //in order to be able to break the text in 5-characters long strings.
+
+           string res = "";
+
+           int p = 0;
+           while (p < s.Length)
+           {
+               for (int j = 0; j < m; j++)
+               {
+                    if (valid(s[p + l2[j] - 1]))    //verifies if the character is in the alphabet
+                        res = res + s[p + l2[j] - 1];
+                    else
+                    {
+                        MessageBox.Show("'" + s[p + l2[j] - 1] + "' is not in the alphabet.");
+                        res = "";
+                         return;
+                    }
+                }
+                p = p + m;
+           }
+
+           txtOut.Text = res;
+
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            string s = txtIn.Text.ToLower();
+            while (s.Length % m != 0)
+                s = s + " ";
+            string res = "";
+
+            //compute the inverse of the permutation
+            List<int> aux = new List<int>(m);
+            for (int i = 0; i < m; i++)
+            {
+                int val = index(i + 1, l2);
+                aux.Add(val + 1);
+            }
+
+            //basically, the whole crypting algorithm is reversed
+            int p = 0;
+            while (p < s.Length)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (valid(s[p + aux[j] - 1]))
+                        res = res + s[p + aux[j] - 1];
+                    else
+                    {
+                        MessageBox.Show("'" + s[p + aux[j] - 1] + "' is not in the alphabet.");
+                        res = "";
+                        return;
+                    }
+                }
+                p = p + m;
+            }
+
+            txtOut.Text = res;
+        }
+
+        private void defaultCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (defaultCheckbox.Checked)
+            {
+                txtn.ReadOnly = true;
+                txtm.ReadOnly = true;
+                txtalpha.ReadOnly = true;
+                loadButton.Enabled = false;
+                encryptButton.Enabled = true;
+                decryptButton.Enabled = true;
+                refreshButton.Enabled = true;
+                loadDefaultAlphabet();
+                txtOut.Text = "";
+            }
+
+            else if (!defaultCheckbox.Checked)
+            {
+                txtn.ReadOnly = false;
+                txtm.ReadOnly = false;
+                txtalpha.ReadOnly = false;
+                txtKey.Text = "";
+                loadButton.Enabled = true;
+                encryptButton.Enabled = false;
+                decryptButton.Enabled = false;
+                refreshButton.Enabled = false;
+                txtOut.Text = "";
+            }
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            //load another permutation
+            l1.Clear();
+            l2.Clear();
+
+            for (int i = 0; i < m; i++)
+                l1.Add(i + 1);
+
+            l2 = ShuffleList<int>(l1);
+
+            txtKey.Text = keyAsString(l2); 
+
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            if (txtn.Text == "" || txtm.Text == "")
+            {
+                MessageBox.Show("Please choose a valid alphabet.");
+                return;
+            }
+
+            try
+            {
+                n = Convert.ToInt16(txtn.Text);
+            }
+
+            catch (Exception) { MessageBox.Show("Please enter a valid number of characters."); return; }
+
+            try
+            {
+                m = Convert.ToInt16(txtm.Text);
+            }
+            catch (Exception) { MessageBox.Show("Please enter a valid order for the permutation."); return; }
+
+            if (m > n)
+            {
+                MessageBox.Show("The order of the permutation must be smaller\nthan the number of characters in the alphabet.");
+                return;
+            }
+            string a = txtalpha.Text.ToLower();
+            if (a.Length != n)
+            {
+                MessageBox.Show("Incorrect number of characters!");
+                return;
+            }
+
+            for (int i = 0; i < a.Length - 1; i++)
+                for (int j = i + 1; j < a.Length; j++)
+                    if (a[i] == a[j])
+                    {
+                        MessageBox.Show("Alphabet cannot contain duplicates! (" + a[i] + ")");
+                        return;
+                    }
+
+            l1 = new List<int>(m);
+            l2 = new List<int>(m);
+
+            for (int i = 0; i < m; i++)
+                l1.Add(i + 1);
+
+            l2 = ShuffleList<int>(l1);
+
+            alpha = new List<char>(n);
+            alpha.Add(' '); //add space by default
+            for (int i = 0; i < n; i++)
+                alpha.Add(a[i]);
+
+            txtKey.Text = keyAsString(l2);
+
+            encryptButton.Enabled = true;
+            decryptButton.Enabled = true;
+            refreshButton.Enabled = true;
+
+        }
+
+        
+ 
+    }
+    }
+
+
---
+++ ElGamal/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Lab_5___ElGamal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool isPrime(BigInteger p)
+        {
+            for (BigInteger i = 2; i < p / 2; i++)
+                if (p % i == 0) return false;
+            return true;
+        }
+
+        private BigInteger genPrime()
+        {
+            Random r = new Random();
+            int a = r.Next()%10000000;
+            while (!isPrime(a))
+                a = r.Next()%10000000;
+            return a;
+        }
+
+        private int chooseGen(int p)
+        {
+            Random r = new Random();
+            int a = r.Next() % 10;
+            while (!isPrime(a))
+                a = r.Next() % 10;
+            return a;
+        }
+
+        public String Encryption(BigInteger p, BigInteger g, BigInteger ga, BigInteger k, String message)
+        {
+            List<BigInteger> m = new List<BigInteger>();
+            List<char> c = new List<char>();
+
+            for (int i = 0; i < message.Length; i++)
+                c.Add(message[i]);
+
+            for (int i = 0; i < c.Count; i++)
+            {
+                m.Add(text2big(c[i]));
+            }
+
+            BigInteger alpha = BigInteger.ModPow(g, k, p);
+            BigInteger beta;
+            String result = alpha.ToString();
+
+            for (int i = 0; i < m.Count ; i++)
+            {
+                beta = (BigInteger.ModPow(ga, k, p) * m[i]) % p;
+                result += " " + beta.ToString();
+            }
+
+            return result;
+        }
+
+        public String Decryption(BigInteger alpha, BigInteger beta, BigInteger a, BigInteger p)
+        {
+
+            int m = (int)((BigInteger.ModPow(alpha, p - 1 - a, p) * beta) % p);
+
+            return big2text(m).ToString();
+        }
+
+        private char big2text(int x)
+        {
+            if (x == 0)
+                return ' ';
+            else
+            {
+                List<char> alpha = new List<char>(27);
+                alpha.Add(' ');
+                for (int i = 1; i < 26; i++)
+                    alpha.Add((char)(96 + i));
+
+                return alpha.ElementAt(x);
+            }
+        }
+
+        private BigInteger text2big(char x)
+        {
+            List<char> alpha = new List<char>(27);
+            alpha.Add(' ');
+            for (int i = 1; i < 26; i++)
+                alpha.Add((char)(96 + i));
+            for (int j = 0; j < alpha.Count; j++)
+                if (x.Equals(alpha.ElementAt(j)))
+                    return j;
+            return 0;
+        }
+
+        private void btnPrimeNumber_Click(object sender, EventArgs e)
+        {
+            txtP.Text = genPrime().ToString();
+        }
+
+        private int Generator(BigInteger p)
+        {
+            BigInteger res = 1, saveP = p - 1;
+            int i = 2;
+            List<int> primes = new List<int>();
+            while (i <= ((p - 1) / 2))
+            {
+                if (isPrime(i))
+                {
+                    while (saveP % i == 0)
+                    {
+                        primes.Add(i);
+                        saveP = saveP / i;
+                    }
+                }
+                if (saveP == 1)
+                {
+                    break;
+                }
+                i++;
+            }
+
+            int[] primes_arr = primes.Distinct().ToArray();
+
+            i = 1;
+            while (i < (p - 1))
+            {
+                int k = 0;
+
+                for (int j = 0; j < primes_arr.Length; j++)
+                {
+                    if (BigInteger.ModPow(i, primes_arr[j], p) == 1)
+                    {
+                        continue;
+                    }
+                    k++;
+                }
+
+                if (k == primes_arr.Length)
+                {
+                    return i;
+                }
+
+                i++;
+            }
+            return 0;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            BigInteger p;
+            BigInteger a;
+            try
+            {
+                p = BigInteger.Parse(txtP.Text);
+                a = BigInteger.Parse(txtA.Text);
+            }
+            catch (Exception) { MessageBox.Show("Please give valid values."); return; }
+
+            if (!isPrime(p))
+            {
+                MessageBox.Show(p + " is not prime.");
+                return;
+            }
+
+            BigInteger g = Generator(p);
+
+            BigInteger b = BigInteger.ModPow(g, a, p);
+
+            txtB.Text = p + " " + g + " " + b;
+            txtPubKey.Text = txtB.Text;
+            txtPrivateKey.Text = txtA.Text;
+            txtPrimeDecr.Text = txtP.Text;
+            txtMes.Clear();
+            txtMesDecr.Clear();
+            txtCipher.Clear();
+            txtCipherDecr.Clear();
+        }
+
+        private void btnCipher_Click(object sender, EventArgs e)
+        {
+            String[] splits = txtPubKey.Text.Split(' ');
+
+            BigInteger p;
+            BigInteger g;
+            BigInteger b;
+            BigInteger k;
+
+            try
+            {
+                p = BigInteger.Parse(splits[0]);
+                g = BigInteger.Parse(splits[1]);
+                b = BigInteger.Parse(splits[2]);
+                k = BigInteger.Parse(txtK.Text);
+            }
+            catch (Exception) { MessageBox.Show("Please give valid values."); return; }
+
+            txtCipher.Text = Encryption(p, g, b, k, txtMes.Text);
+            txtCipherDecr.Text = txtCipher.Text;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            txtMesDecr.Clear();
+
+            String[] splits = txtCipherDecr.Text.Split(' ');
+
+            BigInteger alpha;
+            BigInteger a;
+            BigInteger p;
+            
+            try
+            {
+                alpha = BigInteger.Parse(splits[0]);
+                a = BigInteger.Parse(txtPrivateKey.Text);
+                p = BigInteger.Parse(txtPrimeDecr.Text);
+            }
+            catch (Exception) { MessageBox.Show("Please give valid values."); return; }
+            int i = 1;
+            while (i < splits.Length)
+            {
+                BigInteger beta;
+                try
+                {
+                    beta = BigInteger.Parse(splits[i]);
+                }
+                catch (Exception) { MessageBox.Show("Please give valid values."); return; }
+                txtMesDecr.Text += Decryption(alpha, beta, a, p);
+                i++;
+            }
+        }
+
+        private void txtCipherDecr_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
